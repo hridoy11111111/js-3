9503e100bbcecb942f50d773b91531d49d9ff8cf
HRIDOY CHANDRA  SAHA <145109607+hridoy11111111@users.noreply.github.com>
2023-10-28 10:19:16 +0600

    saha

---
+++ loop.js
@@ -0,0 +1,58 @@
+var jilapiGiven = 0;
+
+while (jilapiGiven < 5) {
+    console.log('jilapi paice');
+     jilapiGiven++ 
+}
+
+var count = 0;
+while (count < 10) {
+    count++
+    console.log('alamin kane dhoro');
+}
+
+var number = 1;
+while (number <=10) {
+  console.log(number);
+  number++  
+}
+
+var number = 2;
+while(number <= 20){
+    console.log(number);
+    number+= 2;
+}
+var number = 1;
+while(number <= 20){
+    console.log(number);
+    number+= 2;
+}
+var number = 2;
+while(number <= 100){
+    console.log(number);
+    number+= 2
+}
+
+var number = 1;
+while(number <=100){
+    console.log(number);
+    number += 2
+}
+
+for(var number = 0; number<=3; number++){
+    console.log(number);
+}
+odd
+for (var i = 2; i <= 20; i+= 2){
+    console.log(i);
+}
+for (var i = 1; i <= 20; i+= 2){
+    console.log(i);
+}
+
+
+var numbers = [10, 12, 15, 20, 25, 30];
+for(var i = 0; i < numbers.length; i++) {
+    var element = numbers[i]
+    console.log(element);
+}
